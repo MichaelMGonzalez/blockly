20e65070c2a348696f51cd1d78670823d8665367
Michael <mmg005@ucsd.edu>
2016-10-16 00:09:22 -0700

    added condition node, more abstraction

--- blockly_behavior_nodes.js
+++ blockly_behavior_nodes.js
@@ -18,7 +18,6 @@ return {
         this.setColour(Blockly.Blocks.logic.HUE);
         this.appendDummyInput()
             .appendField(name);
-
         this.setNextStatement(next);
         this.setPreviousStatement(prev);
         this.contextMenu = false;
@@ -26,12 +25,26 @@ return {
   }
 };
 
-var BehaviorNode = function ( name, color )  {
+var BehaviorNode = function (name, color ) {
+    return {
+        init: function() {
+        this.appendDummyInput()
+            .appendField(name);
+        this.appendValueInput("NAME")
+            .setCheck("Boolean");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setColour(color);
+    }
+    }
+};
+
+var MultiBehaviorNode = function ( name, color )  {
 return {
     init : function() {
         this.appendDummyInput()
             .appendField(name);
-        this.appendStatementInput("Children0")
+        this.appendStatementInput("Child0")
             .setCheck(["selector_node", node_types])
             .appendField(mutator_child);
         this.setPreviousStatement(true, node_types);
@@ -41,12 +54,6 @@ return {
         this.setMutator(new Blockly.Mutator(['behavior_node_extra']));
         this.children_count = 0;
     },
-    
-    /**
-     * Create XML to represent the number of else-if and else inputs.
-     * @return {Element} XML storage element.
-      * @this Blockly.Block
-      */
     mutationToDom: function() {
         if (!this.children_count) {
             return null;
@@ -57,11 +64,6 @@ return {
         }
         return container;
     },
-  /**
-   * Parse XML to restore the else-if and else inputs.
-   * @param {!Element} xmlElement XML storage element.
-   * @this Blockly.Block
-   */
     domToMutation: function(xmlElement) {
         this.children_count = parseInt(xmlElement.getAttribute('children_count'), 10) || 0;
         this.updateShape_();
@@ -78,11 +80,6 @@ return {
         }
         return containerBlock;
         },
-          /**
-   * Reconfigure this block based on the mutator dialog's components.
-   * @param {!Blockly.Block} containerBlock Root block in mutator.
-   * @this Blockly.Block
-   */
   compose: function(containerBlock) {
     var clauseBlock = containerBlock.nextConnection.targetBlock();
     // Count number of inputs.
@@ -106,11 +103,6 @@ return {
       Blockly.Mutator.reconnect(statementConnections[i], this, mutator_child + i);
     }
   },
-  /**
-   * Store pointers to any connected child blocks.
-   * @param {!Blockly.Block} containerBlock Root block in mutator.
-   * @this Blockly.Block
-   */
   saveConnections: function(containerBlock) {
     var clauseBlock = containerBlock.nextConnection.targetBlock();
     var i = 1;
@@ -129,11 +121,6 @@ return {
           clauseBlock.nextConnection.targetBlock();
     }
   },
-  /**
-   * Modify this block to have the correct number of inputs.
-   * @private
-   * @this Blockly.Block
-   */
   updateShape_: function() {
     // Delete everything.
     var i = 1;
@@ -150,31 +137,22 @@ return {
     }
 };
 
-Blockly.Blocks['selector_node'] = new BehaviorNode("(?) Selector Node", 120);
-Blockly.Blocks['sequence_node'] = new BehaviorNode("(->) Sequence Node", 180);
+Blockly.Blocks['selector_node'] = new MultiBehaviorNode("(?) Selector Node", 120);
+Blockly.Blocks['sequence_node'] = new MultiBehaviorNode("(->) Sequence Node", 180);
 Blockly.Blocks['root_node'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("[ ] Root Node");
     this.appendStatementInput("Children")
         .setCheck(node_types);
-    this.setOutput(true, null);
     this.setColour(60);
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
   }
 };
 
-Blockly.Blocks['action_node'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Action Node");
-    this.appendValueInput("NAME")
-        .setCheck("Boolean");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(330);
-  }
-};
+Blockly.Blocks['action_node'] = BehaviorNode( "Action Node", 330 );
+Blockly.Blocks['condition_node'] = BehaviorNode( "Condition Node", 220 );
 
 
 Blockly.Blocks['behavior_node_start'] = mutator( "Number of Child Behaviors", false, true ) ;
